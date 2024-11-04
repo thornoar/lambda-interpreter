92aa80ee4246d773ee5ff873a929b64ba3342141
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-11-04 14:21:37 +0800

    added flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+    inputs = {
+        nixpkgs = {
+            url = "github:NixOS/nixpkgs/nixos-24.05";
+        };
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+        # overlay = final: prev: {
+        #     lambda-interpreter = final.callCabal2nix "lambda-interpreter" ./. { };
+        # };
+        # myHaskellPackages = pkgs.haskellPackages.extend overlay;
+    in
+    {
+        # packages.${system}.default = myHaskellPackages.lambda-interpreter;
+        packages.${system}.default = pkgs.haskellPackages.callCabalnix "lambda-interpreter" ./. { };
+        apps.${system}.default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/lambda-interpreter";
+        };
+    };
+}
