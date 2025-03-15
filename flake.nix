a9e9cc092d36049c9993f1d87beea224b116ad3b
Roman Maksimovich <r.a.maksimovich@gmail.com>
2025-03-15 12:51:49 +0800

    automatic commit

--- flake.nix
+++ flake.nix
@@ -1,25 +1,21 @@
 {
-    inputs = {
-        nixpkgs = {
-            url = "github:NixOS/nixpkgs/nixos-24.05";
-        };
-    };
+  inputs = { nixpkgs = { url = "github:NixOS/nixpkgs/nixos-24.05"; }; };
 
-    outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-        # overlay = final: prev: {
-        #     lambda-interpreter = final.callCabal2nix "lambda-interpreter" ./. { };
-        # };
-        # myHaskellPackages = pkgs.haskellPackages.extend overlay;
-    in
-    {
-        # packages.${system}.default = myHaskellPackages.lambda-interpreter;
-        packages.${system}.default = pkgs.haskellPackages.callCabal2nix "lambda-interpreter" ./. { };
-        apps.${system}.default = {
-            type = "app";
-            program = "${self.packages.${system}.default}/bin/lambda-interpreter";
-        };
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      # overlay = final: prev: {
+      #     lambda-interpreter = final.callCabal2nix "lambda-interpreter" ./. { };
+      # };
+      # myHaskellPackages = pkgs.haskellPackages.extend overlay;
+    in {
+      # packages.${system}.default = myHaskellPackages.lambda-interpreter;
+      packages.${system}.default =
+        pkgs.haskellPackages.callCabal2nix "lambda-interpreter" ./. { };
+      apps.${system}.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/lambda-interpreter";
+      };
     };
 }
